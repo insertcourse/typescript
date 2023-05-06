ec1c5a9df32fa4d16fe16907e9430e68cd9a4245
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-05-07 05:01:43 +0900

    ADD : 미션 1

--- practice/practice1.ts
+++ practice/practice1.ts
@@ -12,6 +12,18 @@ interface PersonType {
     job? : string //?를 붙이면 선택적인 요소
 }
 
+//객체 구조 분해 할당
+const PersonType ={
+    name1: "siyeon",
+    age: 17,
+    gender: {
+        female : "F",
+        male : "M"
+    },
+    job:"student"
+}
+const {name1, age, gender:{female}, job} = PersonType
+console.log(PersonType)
 
 //arrow function : 화살표 함수
 //형식) const 변수명 = (매개변수명 : 자료형) =>{
@@ -23,3 +35,17 @@ const person =(name:number, age:string, gender:string, job:string) => {
     console.log(gender)
     console.log(job)
 }
+
+
+
+
+
+//밑에는 자주 보려고 써놨습니다...
+
+// 구조 분해 할당을 사용하는 이유
+// [객체와 배열은 js에서 가장 많이 쓰이는 자료형]
+// - 객체사용 : key를 가진 데이터 여러개를 하나로 묶어서 저장할때
+// - 배열사용 : 컬렉션에 테이터를 순서대로 저장할때
+// + 함수에 객체나 배열에 저장된 데이터 전체가 아닌, 일부만 전달할때
+// + 함수의 매개변수가 많을때
+// + 매개변수의 기본값이 필요한 경우에도 사용할수있음
