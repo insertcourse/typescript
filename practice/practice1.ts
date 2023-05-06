e45dd1b9b72b5262c1b9cc89a0f89e3fd15da091
youngeunkim <pkhj286@gmail.com>
2023-05-07 03:01:33 +0900

    ADD : 미션 1

--- practice/practice1.ts
+++ practice/practice1.ts
@@ -1,3 +1,16 @@
-interface PersonType {}
+interface PersonType {
+    name: string;
+    age:number;
+    gender: 'F' | 'M';
+    job?: string; //선택적인 요소!!!
+}
 
-const person = {}
+
+const person = ({ name, age, gender, job }: PersonType) => {
+    console.log(`이름: ${name}`);
+    console.log(`나이: ${age}`);
+    console.log(`성별: ${gender}`);
+    if (job) {
+    console.log(`직업: ${job}`);
+    }
+};
