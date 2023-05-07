93fc57f0a725dc71d2e88ddf263ed1ba444d9c77
youngeunkim <pkhj286@gmail.com>
2023-05-07 21:38:33 +0900

    ADD : 변수 선언

---
+++ todolist/script.ts
@@ -0,0 +1,5 @@
+const button = document.getElementById('inputForm--button') as HTMLButtonElement; // +버튼
+const add = document.getElementById('inputForm--add') as HTMLInputElement; //todolist추가
+const remove = document.getElementById('delete') as HTMLButtonElement; //모두삭제
+const result = document.getElementById('addTodo--text') as HTMLElement //입력 창 
+
