e17767936b86a1cad77ef5d2bbb198983c80346f
youngeunkim <pkhj286@gmail.com>
2023-05-07 20:44:29 +0900

    ADD : 미션 2 filter

--- practice/practice2.ts
+++ practice/practice2.ts
@@ -5,10 +5,18 @@
 //     }
 // ) //배열에 8을 곱한 값인 16,32,48,64,80 이 반환됨.
 
-//forEach 한 개 씩 무언가 하는것..!!
-['cute','pretty','youngeun'].forEach (
-    function(yeah) {
-        console.log(yeah);
+// //forEach 한 개 씩 무언가 하는것..!!
+// ['cute','pretty','youngeun'].forEach (
+//     function(yeah) {
+//         console.log(yeah);
+//     }
+// )
+// //cute, pretty, youngeun이 차례대로 출력 ㅎㅎ
+
+//filter 배열에서 조건에 맞는 것만 반환하기
+[1,5,10,15,20].filter (
+    function(division){
+        return division % 4 === 0;
     }
 )
-//cute, pretty, youngeun이 차례대로 출력 ㅎㅎ
+//[20] 배열이 반환된다.
