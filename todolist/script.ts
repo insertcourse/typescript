3982a1707f62026259426f3851d8154e303d8613
youngeunkim <pkhj286@gmail.com>
2023-05-07 21:43:46 +0900

    ADD : 변수 타입 지정

--- todolist/script.ts
+++ todolist/script.ts
@@ -1,5 +1,5 @@
-const button = document.getElementById('inputForm--button') as HTMLButtonElement; // +버튼
-const add = document.getElementById('inputForm--add') as HTMLInputElement; //todolist추가
-const remove = document.getElementById('delete') as HTMLButtonElement; //모두삭제
-const result = document.getElementById('addTodo--text') as HTMLElement //입력 창 
+const button: HTMLButtonElement = document.getElementById('inputForm--button') as HTMLButtonElement; // +버튼
+const add: HTMLInputElement = document.getElementById('inputForm--add') as HTMLInputElement; //todolist추가
+const remove: HTMLButtonElement = document.getElementById('delete') as HTMLButtonElement; //모두삭제
+const result: HTMLElement = document.getElementById('addTodo--text') as HTMLElement //입력 창 
 
