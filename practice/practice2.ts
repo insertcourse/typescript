0d536aa1855a1bdc5ed9c4d98009587eafa5984b
youngeunkim <pkhj286@gmail.com>
2023-05-07 19:55:19 +0900

    ADD :  미션 2 forEach

--- practice/practice2.ts
+++ practice/practice2.ts
@@ -1,7 +1,14 @@
-//map 배열의 각 요소에 어떤 행위를 한 결과를 새 배열로 반환.
-[2,4,6,8,10].map(
-    function(e){
-        return e*8;
-    }
-) //배열에 8을 곱한 값인 16,32,48,64,80 이 반환됨.
+// //map 배열의 각 요소에 어떤 행위를 한 결과를 새 배열로 반환.
+// [2,4,6,8,10].map(
+//     function(e){
+//         return e*8;
+//     }
+// ) //배열에 8을 곱한 값인 16,32,48,64,80 이 반환됨.
 
+//forEach 한 개 씩 무언가 하는것..!!
+['cute','pretty','youngeun'].forEach (
+    function(yeah) {
+        console.log(yeah);
+    }
+)
+//cute, pretty, youngeun이 차례대로 출력 ㅎㅎ
