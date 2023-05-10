a66498933ffa4cba1ef1bb6e4d82568531fbca0f
우빈 <102154880+Ubinquitous@users.noreply.github.com>
2023-05-10 17:01:29 +0900

    Revert "[타입스크립트 개념잡기] 김영은 미션 제출합니다."

--- practice/practice1.ts
+++ practice/practice1.ts
@@ -1,16 +1,3 @@
-interface PersonType {
-    name: string;
-    age:number;
-    gender: 'F' | 'M';
-    job?: string; //선택적인 요소!!!
-}
+interface PersonType {}
 
-
-const person = ({ name, age, gender, job }: PersonType) => {
-    console.log(`이름: ${name}`);
-    console.log(`나이: ${age}`);
-    console.log(`성별: ${gender}`);
-    if (job) {
-    console.log(`직업: ${job}`);
-    }
-};
+const person = {}
